dca2395348867a2fd18f9815a55ee289b597e762
bubas3000 <47362581+bubas3000@users.noreply.github.com>
2022-03-04 02:19:07 +0000

    Update index.php

--- index.php
+++ index.php
@@ -377,7 +377,48 @@
 
 
 
+				<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+								<style>
+								.mySlides {display:none;}
+								</style>
+								<div id="fh5co-when-where" class="fh5co-section-gray">
+									<div class="container">
+								
+								<h2 class="w3-center">Manual Slideshow</h2>
+								
+								<div class="w3-content w3-display-container">
+									<img class="mySlides" src="images/xia4 (Custom).jfif" style="width:100%">
+									<img class="mySlides" src="images/Xia (Custom).png" style="width:100%">
+									<img class="mySlides" src="images/xia6 (Custom).jpg" style="width:100%">
+									<img class="mySlides" src="images/Xia1lado (Custom).jpeg" style="width:100%">
+								
+								  <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+								  <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+								</div>
+								
+								<script>
+								var slideIndex = 1;
+								showDivs(slideIndex);
+								
+								function plusDivs(n) {
+								  showDivs(slideIndex += n);
+								}
+								
+								function showDivs(n) {
+								  var i;
+								  var x = document.getElementsByClassName("mySlides");
+								  if (n > x.length) {slideIndex = 1}
+								  if (n < 1) {slideIndex = x.length}
+								  for (i = 0; i < x.length; i++) {
+									x[i].style.display = "none";  
+								  }
+								  x[slideIndex-1].style.display = "block";  
+								}
+								</script>
+									</div>
+								</div>
 
+								
 
 
 
